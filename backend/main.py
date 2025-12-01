970565bb961089c3c952fa3271263d5a69505fe7
Ziheng <zlia0050@student.monash.edu>
2025-12-02 00:08:22 +1100

    updated CORS: backend

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,9 @@ start_scheduler()
 # CORS so React can talk to backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173",
+                   "https://pollen-forecast.vercel.app/"],
+    allow_credentials=True,  
     allow_methods=["*"],
     allow_headers=["*"]
 )
