40a5ebd73cb3a681201c429a6fd3f8a4aa4af439
Ziheng <zlia0050@student.monash.edu>
2025-12-02 00:47:07 +1100

    pollen tile timeout

--- backend/app/cache/mongo.py
+++ backend/app/cache/mongo.py
@@ -22,20 +22,26 @@ class TilesCache(CacheManager):
     def save_cache(self, tile_type, z, x, y, data):
         self.collection.update_one(
             {"_id": f"{tile_type}_{z}_{x}_{y}"},
-            {"$set": {"data": data}},
+            {"$set": {"data": data}, "timestamp": time.time()},
             upsert=True
         )
 
     def load_cache(self, tile_type, z, x, y):
         doc = self.collection.find_one({"_id": f"{tile_type}_{z}_{x}_{y}"})
-        return doc["data"] if doc else None
+        if not doc:
+            return None
+
+        if time.time() - doc["timestamp"] > self.TILE_TTL:
+            return None
+
+        return doc["data"]
     
 
 class ForecastCache(CacheManager):
     def __init__(self):
         super().__init__()
         self.collection = self.db["forecast_cache"]
-        self.FORECAST_TTL = 60 * 60       # 1 hour
+        self.FORECAST_TTL = 60 * 60 * 6      # 6 hours
     
     def load_cache(self, geohash):
         key = geohash
